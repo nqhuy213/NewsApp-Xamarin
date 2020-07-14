ebd6e5b9cba63ba69cb11c334b0ee93ff9ebd631
Huy <nqhuy213@gmail.com>
2020-07-15 09:17:54 +1000

    Create Mock Views and ViewModels

---
+++ NewsApp/NewsApp/ViewModels/ViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace NewsApp.ViewModels
+{
+    public abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
