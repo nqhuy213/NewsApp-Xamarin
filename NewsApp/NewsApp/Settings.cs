0cbe51161309869df5666340b428ebc57351ce14
Huy <nqhuy213@gmail.com>
2020-07-15 10:24:11 +1000

    Implement NewsService

--- NewsApp/NewsApp/Settings.cs
+++ NewsApp/NewsApp/Settings.cs
@@ -6,6 +6,6 @@ namespace NewsApp
 {
     public static class Settings
     {
-        public static string NewsApiKeys = "36a923565f8e44d9b4e6cb7b606fccff";
+        public static string NewsApiKey = "36a923565f8e44d9b4e6cb7b606fccff";
     }
 }
--- NewsApp/NewsApp/ViewModels/HeadlinesViewModel.cs
+++ NewsApp/NewsApp/ViewModels/HeadlinesViewModel.cs
@@ -1,14 +1,47 @@
-﻿using System;
+﻿using NewsApp.Models;
+using NewsApp.Services;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace NewsApp.ViewModels
 {
     public class HeadlinesViewModel: ViewModel
     {
-        public HeadlinesViewModel()
+        private readonly NewsService Service;
+        public NewsResult CurrentNews { get; set; }
+        public HeadlinesViewModel(NewsService service)
         {
-
+            this.Service = service;
         }
+
+        public async Task Initialize(NewsScope scope)
+        {
+            CurrentNews = await Service.GetNews(scope);
+        }
+
+        public async Task Initialize(string scope)
+        {
+            var resolvedScope = scope.ToLower() switch
+            {
+                "local" => NewsScope.Local,
+                "global" => NewsScope.Global,
+                "headlines" => NewsScope.Headlines,
+                _ => NewsScope.Headlines
+            };
+            await Initialize(resolvedScope);
+        }
+
+        public ICommand ItemSelected =>
+            new Command(async (selectedItem) =>
+            {
+                var selectedArticle = selectedItem as Article;
+                var url = HttpUtility.UrlEncode(selectedArticle.Url);
+
+            });
     }
 }
